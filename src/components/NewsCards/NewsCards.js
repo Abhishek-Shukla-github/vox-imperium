59646378dc3bc6ff8b781c6e6e03d189a034c8e5
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-11-06 19:30:58 +0530

    Style the basic Card template

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,11 +1,18 @@
 import React from "react";
 import NewsCardStyle from "../NewsCard/NewsCardStyle";
+import { Grid,Typography,Grow } from "@material-ui/core";
 
 const NewsCards = ({articles}) => {
     return (
-        <div>
-            {articles.map((article,i)=>(<NewsCardStyle />))}
-        </div>
+        <Grow in>
+            <Grid spacing={3} container alignItems="stretch">
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} style={{display:"flex"}}>
+                        <NewsCardStyle article={article} i={i}/>
+                    </Grid>
+                ))}
+            </Grid>
+        </Grow>
     );
 } 
 

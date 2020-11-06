0cffc39221cea4452401887211b0ec982e9f17c4
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-11-06 22:55:38 +0530

    Style the container using makeStyle and useStyle

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,11 +1,12 @@
 import React from "react";
 import NewsCardStyle from "../NewsCard/NewsCardStyle";
-import { Grid,Typography,Grow } from "@material-ui/core";
-
-const NewsCards = ({articles}) => {
+import { Grid, Typography, Grow } from "@material-ui/core";
+import useStyles from "./styles.js";
+const NewsCards = ({ articles }) => {
+    const classes = useStyles();
     return (
         <Grow in>
-            <Grid spacing={3} container alignItems="stretch">
+            <Grid spacing={3} className={classes.container} container alignItems="stretch">
                 {articles.map((article, i) => (
                     <Grid item xs={12} sm={6} md={4} lg={3} style={{display:"flex"}}>
                         <NewsCardStyle article={article} i={i}/>
---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles({
+    container: {
+        padding: "0 5%",
+        margin: 0,
+        width: "100%",
+    }
+})

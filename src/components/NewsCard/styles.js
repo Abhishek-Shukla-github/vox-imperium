bf8dfebc11b3dae8dd44d7fcb59522409c3321b5
Abhishek Shukla <abhishekshuklaratedr@gmail.com>
2020-11-10 15:04:45 +0530

    Fix activeCard CSS bug

--- src/components/NewsCard/styles.js
+++ src/components/NewsCard/styles.js
@@ -3,8 +3,6 @@ export default makeStyles({
     image: {
         height: 250,
     },
-    title: {
-    },
     card: {
         flexDirection: "column",
         display: "flex",
@@ -17,14 +15,14 @@ export default makeStyles({
     details: {
         display: "flex",
         justifyContent: "space-between",
-        margin:"10px"
+        margin:"10px",
     },
     cardActions: {
         display: "flex",
         justifyContent: "space-between",
-        padding:"0 16px"
+        padding:"0 16px",
     },
     activeCard:{
         boxShadow: "0 0 10px 5px #0047AB ",
-    }
+    },
 })
